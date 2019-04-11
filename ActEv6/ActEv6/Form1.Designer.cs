24cc921033a39761ca284e41ce20dbab3d2695d8
glsegu <glsegu@floridauniversitaria.es>
2019-04-11 18:23:23 +0200

    cambio etiquetas de fecha y hora

--- ActEv6/ActEv6/Form1.Designer.cs
+++ ActEv6/ActEv6/Form1.Designer.cs
@@ -59,9 +59,9 @@
             this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHora.Location = new System.Drawing.Point(615, 59);
             this.lblHora.Name = "lblHora";
-            this.lblHora.Size = new System.Drawing.Size(118, 42);
+            this.lblHora.Size = new System.Drawing.Size(99, 42);
             this.lblHora.TabIndex = 1;
-            this.lblHora.Text = "label1";
+            this.lblHora.Text = "Hora";
             this.lblHora.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
@@ -76,9 +76,9 @@
             this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFecha.Location = new System.Drawing.Point(518, 27);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(60, 24);
+            this.lblFecha.Size = new System.Drawing.Size(64, 24);
             this.lblFecha.TabIndex = 2;
-            this.lblFecha.Text = "label1";
+            this.lblFecha.Text = "Fecha";
             // 
             // btnMantenimiento
             // 
