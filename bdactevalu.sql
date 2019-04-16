571ae5ac8565e5928b62c2632bfe5beeaa59bf71
EQUIPO\Glorieta <gloriasedo4@gmail.com>
2019-04-17 01:06:38 +0200

    subo la nueva bd

---
+++ bdactevalu.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE bdactevalu CHARACTER SET UTF8;
+
+USE bdactevalu;
+
+CREATE TABLE empleados (
+ID VARCHAR() NOT NULL PRIMARY KEY,
+NIF varchar(9) NOT NULL PRIMARY KEY,
+nombre varchar(50) NOT NULL,
+apellido varchar(100) NOT NULL,
+administrador tinyint(1) NOT NULL,
+claveAdmin varchar(10) DEFAULT NULL
+);
+
+
+CREATE TABLE fichajes (
+NIFempleado varchar(9) NOT NULL,
+dia date NOT NULL,
+horaEntrada datetime NOT NULL,
+horaSalida datetime NOT NULL,
+fichadoEntrada tinyint(1) NOT NULL,
+fichadoSalida tinyint(1) NOT NULL
+)
